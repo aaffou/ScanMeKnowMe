71cf42d61298fd27196bfac232bfc171e630e84e
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-04-04 13:22:07 +0200

    lanch app and start front

---
+++ src/components/banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="banner">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .banner {
+    height: 30vh;
+    width: 100vw;
+    background-color: red;
+  }
+</style>
---
+++ src/components/card-object.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card-obj">
+      <div class="img-obj"></div>
+      <h3 class="title-obj">Cookeo Touch</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  .card-obj {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 30vh;
+    width: 90vw;
+    border-radius: 24px;
+    z-index: 1;
+    background-color: blue;
+    position: absolute;
+    top: 20vh;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    .img-obj {
+      height: 20vh;
+      width: 20vh;
+      border-radius: 12px;
+      background-color: red;
+    }
+    .title-obj {
+
+    }
+  }
+</style>
