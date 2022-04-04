8902c1b0ed124949224d57e01b655ebee1d5b2f4
Affou Adam <aaffou@groupeseb.com>
2022-04-04 15:26:23 +0200

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <template>
-  <main>
-    <navbar></navbar>
-    <banner></banner>
-    <cardObject></cardObject>
-    <cardsAction></cardsAction>
-  </main>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import navbar from './components/navbar.vue'
-import banner from './components/banner.vue'
-import cardObject from './components/card-object.vue'
-import cardsAction from './components/cards-action.vue'
-export default {
-  name: 'App',
-  components: {
-    navbar, banner, cardObject, cardsAction
-  }
-}
-</script>
-
 <style lang="scss">
-  body {
+ body {
     margin: 0;
-    main {
-      height: 100vh;
-      width: 100vw;
-    }
   }
 </style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <navbar></navbar>
+    <banner></banner>
+    <cardObject></cardObject>
+    <cardsAction></cardsAction>
+  </main>
+</template>
+
+<script>
+import navbar from '../components/navbar.vue'
+import banner from '../components/banner.vue'
+import cardObject from '../components/card-object.vue'
+import cardsAction from '../components/cards-action.vue'
+export default {
+  name: 'product',
+  components: {
+    navbar, banner, cardObject, cardsAction
+  }
+}
+</script>
+
+<style lang="scss">
+ main {
+      height: 100vh;
+      width: 100vw;
+    }
+</style>
